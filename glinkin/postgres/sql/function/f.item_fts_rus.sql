0314c46bf6ac532fe156f797df408bea008b81c8
Глинкин Денис Дмитриевич <glinkindd@gmail.com>
2018-10-18 12:10:57 +0300

    postgres fts

---
+++ glinkin/postgres/sql/function/f.item_fts_rus.sql
@@ -0,0 +1,15 @@
+-- Usage:
+-- select * from item_fst_rus('книги для детей 100 способов');
+
+create or replace function item_fst_rus(query text)
+  returns table (name text, annotation text)
+as
+$body$
+  SELECT name, annotation
+  FROM item,
+       plainto_tsquery('russian', $1) AS q,
+       ts_rank(make_tsvector(name, annotation), q) AS rnk
+  WHERE make_tsvector(name, annotation) @@ q
+  ORDER BY rnk DESC limit 10;
+$body$
+language sql;
--- glinkin/postgres/sql/table/t.item_json.sql
+++ glinkin/postgres/sql/table/t.item_json.sql
@@ -1,3 +1,6 @@
 create table item_json (
     data jsonb not null
 );
+
+create index if not exists idx_fts_item_json on item_json
+  using gin(make_tsvector(data->>'name', data->>'annotation'));
