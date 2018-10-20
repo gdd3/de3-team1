83cfc357027f59508ed4a2edd0b2974ae80b8802
Глинкин Денис Дмитриевич <glinkindd@gmail.com>
2018-10-20 22:16:35 +0300

    remove mapping from esbulk scripts

--- glinkin/elastic/script/esbulk_item.sh
+++ glinkin/elastic/script/esbulk_item.sh
@@ -1,13 +1,13 @@
 # Requirements: jq, esbulk (https://github.com/miku/esbulk)
 
-cat data/item_details_full.json | \
+cat ~/data/item_details_full.json | \
   jq  -c '.["annotation"] = .attr0 | .["name"] = .attr1 | .["author"] = .attr2 | {annotation, name, author, itemid, parent_id}' | \
-  esbulk -verbose -index item -mapping scripts/mapping.json -id itemid
+  esbulk -verbose -index item -id itemid
 
-esbulk -verbose -index item -mapping scripts/mapping.json data/ratings.json -id itemid
+esbulk -verbose -index item ~/data/ratings.json -id itemid
 
-esbulk -verbose -index item -mapping scripts/mapping.json data/catalogs.json -id itemid
+esbulk -verbose -index item ~/data/catalogs.json -id itemid
 
-cat data/catalog_path.json | \
+cat ~/data/catalog_path.json | \
   jq -c '.["catalogpath"] = (.catalogpath|tostring)' | \
-  esbulk -verbose -index item -mapping scripts/mapping.json -id catalogid
+  esbulk -verbose -index item -id catalogid
