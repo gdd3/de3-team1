db75925157502cb0bc4fdafee047987755358c4d
Evgenii Prusov <prusove@gmail.com>
2018-10-28 14:35:32 +0300

    add new params to mapping

--- project-03/mapping.groovy
+++ project-03/mapping.groovy
@@ -1,7 +1,14 @@
 mapping {
-    map remoteHost() onto 'remoteHost'
+    map duplicate() onto 'detectedDuplicate'
+    map corrupt() onto 'detectedCorruption'
     map firstInSession() onto 'firstInSession'
     map timestamp() onto 'timestamp'
     map clientTimestamp() onto 'clientTimestamp'
+    map remoteHost() onto 'remoteHost'
     map referer() onto 'referer'
+    map location() onto 'location'
+    map partyId() onto 'partyId'
+    map sessionId() onto 'sessionId'
+    map pageViewId() onto 'pageViewId'
+    map eventType() onto 'eventType'
 }
