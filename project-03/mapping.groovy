437e8c814f664050e1fbec0c3a319dfbfea3b3a0
Evgenii Prusov <prusove@gmail.com>
2018-10-28 10:37:06 +0000

    more attr to test deserialization

--- project-03/mapping.groovy
+++ project-03/mapping.groovy
@@ -1,3 +1,7 @@
 mapping {
-  map remoteHost() onto 'remoteHost'
+    map remoteHost() onto 'remoteHost'
+    map firstInSession() onto 'firstInSession'
+    map timestamp() onto 'timestamp'
+    map clientTimestamp() onto 'clientTimestamp'
+    map referer() onto 'referer'
 }
