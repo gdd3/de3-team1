205dafde17c6a381f6fc63422ef0b12f7255eb9c
Глинкин Денис Дмитриевич <glinkindd@gmail.com>
2018-11-01 11:58:40 +0300

    check if no data found

--- glinkin/project_02/pipeline/batch_event.sh
+++ glinkin/project_02/pipeline/batch_event.sh
@@ -1,2 +1,6 @@
-python event_consumer.py | \
-  clickhouse-client -n --query="SET input_format_skip_unknown_fields=1; INSERT INTO event FORMAT JSONEachRow"
+tmp_data_path="/tmp/events.json"
+
+python event_consumer.py > ${tmp_data_path}
+[ -s ${tmp_data_path} ] && cat ${tmp_data_path} | \
+  clickhouse-client -n --query="SET input_format_skip_unknown_fields=1; INSERT INTO event FORMAT JSONEachRow" || \
+  echo "No data to insert"
