8a97c1d222add2c33664f15bdb9deedc3949328c
Petrov Bogdan <BPetrov@optimum.omd.ru>
2018-11-06 11:19:10 +0300

    Загрузил скрипты и конфиги для второй лабораторной

---
+++ petrov/lab2/lab2_mapping.groovy
@@ -0,0 +1,8 @@
+mapping {
+  map timestamp() onto 'timestamp'
+  map eventType() onto 'eventType'
+  map location() onto 'location'
+  map sessionId() onto 'sessionId'
+  map eventParameter('price') onto 'price'
+  map eventParameter('amount') onto 'amount'
+}
