7b195eabca7aa7cc6b0a98e25cd2d88f8f3e3911
Глинкин Денис Дмитриевич <glinkindd@gmail.com>
2018-11-14 20:49:36 +0300

    remove old

--- glinkin/project_03/clickhouse/objects/v.order_count.sql
+++
@@ -1,7 +0,0 @@
--- old
-create view order_count as
-select toStartOfFiveMinute(eventTime) as start_dt
-     , count(if(eventType == 'checkoutEvent', 1, null)) as order_count
-from event
-group by start_dt
-order by start_dt;
--- glinkin/project_03/clickhouse/objects/v.user_count.sql
+++
@@ -1,7 +0,0 @@
--- old
-create view user_count as
-select toStartOfFiveMinute(eventTime) as start_dt
-     , uniq(remoteHost, sessionId, userAgentName) as user_count
-from event
-group by start_dt
-order by start_dt;
